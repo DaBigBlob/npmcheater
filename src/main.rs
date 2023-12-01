a4e8d86ea51e3eaaaa87fd53afcc3418d2586b3e
Person <localboxcrox@gmail.com>
2023-12-01 12:32:31 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,38 @@ mod sleep;
 mod logs;
 
 use reqwest;
+use clap::Parser;
 use utils::fetch_pkg;
 use sleep::wait_rand_sec;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct ClapCli {
+    #[arg(short, long)]
+    silent: Option<bool>,
+
+    #[arg(short, long)]
+    packages: Option<Vec<String>>,
+
+    #[arg(short, long = "max-sleep-mili")]
+    max_sleep_mili: Option<u64>,
+
+    #[arg(short, long = "user-agent")]
+    user_agent: Option<String>
+}
+
+
 fn main() {
+    let args = ClapCli::parse();
+
+    //user agent
+    let user_agent = match args.user_agent {
+        Some(ua) => ua,
+        None => String::from("npm@10.2.0/node@v21.1.0+arm64 (darwin)")
+    };
+
     let npm_client = match reqwest::blocking::Client::builder()
-    .user_agent("npm@10.2.0/node@v21.1.0+arm64 (darwin)")
+    .user_agent(user_agent)
     .brotli(true)
     .deflate(true)
     .gzip(true)

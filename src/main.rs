d890b4af7e78c90e71ef2c85f29e44695aa157fd
Person <localboxcrox@gmail.com>
2024-01-05 04:37:43 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,10 @@ fn main() {
 
     loop {
         queue.iter().for_each(|elm| {
-            match fetcher::TarDist::get(&elm.pkg(), &client, elm.tar_url()) {
+            match fetcher::TarDist::get(&elm.pkg(), client, elm.tar_url()) {
                 Ok(d) => {
                     log::info!("url fetched for {}", elm.pkg());
-                    match fetcher::TarBall::get(d.url(), &client, elm.tar_ball()) {
+                    match fetcher::TarBall::get(d.url(), client, elm.tar_ball()) {
                         Ok(_) => log::info!("tarball fetched for {}", elm.pkg()),
                         Err(_) => log::warn!("tarball for {} could not be fetched", elm.pkg())
                     };
